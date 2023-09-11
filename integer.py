ee627be9d59381db8b1d7d463ace03664379a443
ltbka <144647920+ltbka@users.noreply.github.com>
2023-09-11 13:49:16 +0600

    Add files via upload

---
+++ integer.py
@@ -0,0 +1,276 @@
+строки -неизменяемый тип данрых который предназначен для хранения текста
+заключаются в одинарные или двойнфе ковычки
+
+
+string``
+
+
+
+
+
+print(string4)
+
+string=```
+tekst` `` ``  ``` 
+
+ тут можно ставить любые кавычки
+ 
+ 
+ 
+ 
+ 
+a=`Hello`
+b =` World`
+result=a + b
+print(result)
+
+
+дублирование строк
+a=`Hello`
+print()
+print(a ,`ошибка`)
+
+
+`==========Формативнфые Строк===========
+
+Форматирование строк
+1.С использованием 
+2.С использованием метода /format()
+3.интерполяция строк(ф.строки)
+
+
+
+
+
+
+
+
+
+print(`Hello ,`,+,)
+                     
+                
+                
+        
+        
+# result=`hello,(}, , ,`.format(name1,name2,name3)
+result=`Hello, `
+
+
+
+
+
+
+ name= input() 
+ 
+ 
+ 
+ 
+ методы строк
+ 
+ мктоды типов данных -функции б к которым мы оброщаемся чрез точку
+ 
+ методы на is 
+ все что начинается на  возвращает True/False
+ 
+ s
+ tring =`123123`
+ 
+ isdigit()возвращает   если строка полностью состоит из букв
+        ()состоит ли строка из цифр или букв
+ print(string.isalpha(())
+       
+       
+       
+
+string=`Asd123`
+islofer()
+print(string.islower())
+
+isupper()
+isupper()
+isspace()
+string=`Asd123Tasdasd`
+
+
+istitle()
+
+print(string.istitle(()
+
+print())
+      
+      
+      
+
+`Hello,lower(`hello`)
+
+
+
+
+
+
+
+
+
+a=`asdasdaJJKK`
+printa
+
+swapcase() -
+
+a=`Hello,World!`
+print.()
+from django.utils.translation import ugettext_lazy as _
+
+
+
+a=`HELLO`
+printa(a.title())
+
+strip()-
+
+a=  `  Hello world!
+print(a.strip())
+
+lstrip() rstrip()- убирает 
+
+replace(old,new) - меняет значение  на 
+
+a=`ha ha ha ha ha ha`
+a.replace(`ha`,`he`)
+print(a)
+print(a,replace(`ha`, `he`))
+
+split() -делит строку по разделениям т возвращает массив  list()
+    
+ a=`Hello,Wor,ld!`
+ b=a.split(`,`)
+ print(b)
+ 
+ 
+ 
+ #индексы 
+ 
+ #индекс -
+ 
+ 
+    
+    
+
+
+
+# # h e l l o w o r l d
+# # 0 2 3 4 5  6 7 8  9
+
+#print(string[10])
+# string[5]
+
+# 
+# string[star:end:step]
+
+string=`hello world`
+
+# print(string[0:5])
+# print(string[0:6])
+# print([:])
+# print(string[:])
+# print(string[0:5][2:4][:1])
+# string(
+    
+# string[:5]
+
+
+
+
+
+# print(string[::]) 
+
+
+
+
+
+# capitalize()
+
+print(`hello`.capaitalize())
+
+endswith(element) 
+
+# a= `hello world!` 
+# print(a.endswith(`!`))
+
+# starswich(element) -
+
+# a=`makers`
+# prin(a.count()`k`,2)
+
+
+# a=`eiphone`
+#print(a.starwich)
+
+# len() - 
+
+
+
+
+# a=`hello`
+# print(len(a))
+
+
+# index()
+
+# a=`hello world`
+# print(a.index(`a`))
+
+# find()
+
+
+# a*=* 
+
+
+# id()
+
+# a=`hello`
+# b=`hello`
+# print 
+# print
+
+# list1[1,2,3,4]
+# list2[1,2,3,5]
+# print(id(list1))
+# print(id(list2))
+
+# join()
+
+text =``Milk,Bread,Water``
+# [Milk, Bread, Water]
+# split =text.split(`,`) [`Milk`,`Bread`,`Water`]
+# joined =
+
+
+
+
+# 1 обменяйте первый и последние элемен
+
+
+
+
+
+
+# 2. imput:address =``1.1.1.1``
+# Output:``1[.]1[.]1[.]``
+
+address=``1.1.1.1``
+print(address.replace
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
